6b4d1c02603bac744c415bbd0ea61dc62c49f13f
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2020-05-24 13:36:12 +0300

    SpecialMove

--- FootCommander.hpp
+++ FootCommander.hpp
@@ -27,13 +27,21 @@ public:
 
         std::pair <int,int> placeToAttack = {0,0};
         int minDistance = INT16_MAX;
-        for (size_t i = 0; i < 8; ++i)
+        for (int i = 0; i < board.size(); ++i)
         {
-            for (size_t j = 0; j < 8; ++j)
+            for (int j = 0; j < board[i].size(); ++j)
             {
                 if (board[i][j] != nullptr && board[i][j]->playerNum == PlayerToAttack){//if soldier of attacked player found
-                    if(distance(placeOfSoldier.first, placeOfSoldier.second, i, j) < minDistance)//calculate distance
-                        placeToAttack = {i,j};//save the place of the closest enemy's soldier
+                    {
+                        float Dis = distance(placeOfSoldier.first, placeOfSoldier.second, i, j);
+                        if(Dis < minDistance)//calculate distance
+                        {
+                            placeToAttack = {i,j};//save the place of the closest enemy's soldier
+                            minDistance = Dis;
+                        }
+
+                    }
+
                 }
             }
 
@@ -43,6 +51,7 @@ public:
         if (minDistance < INT16_MAX){
             board[x][y]->decreaseHealth(10);
             std::cout << "FootCommander:Attack:Reducing health of enemy at: " << x << "," << y << std::endl;
+            std::cout << "EnemeySoldier at: " << x << " ," << y << " Health is: " << board[x][y]->health_points << std::endl;
         }
     }
     // Function to calculate distance
