67f40670a803c061e02ff1f48c7d79b297889723
Shahar Niknazar <57103646+ShaharNik@users.noreply.github.com>
2020-06-04 21:00:05 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -9,10 +9,10 @@
 #include "DemoGame.hpp"  
 
 using namespace std;
-/*
+
 int main() {
 	WarGame::DemoGame demoGame;  // Initialize a game on a 8-by-8 board
 	cout << "The winner is:\n " << demoGame.play() << endl;
     return 0;
 }
-*/
+
