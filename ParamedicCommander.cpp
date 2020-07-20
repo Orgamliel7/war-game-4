1f7462e372ccf41b569f4e354582118b5958535f
Orgamliel7 <57177424+Orgamliel7@users.noreply.github.com>
2020-07-20 16:32:40 +0300

    Update ParamedicCommander.cpp

--- ParamedicCommander.cpp
+++ ParamedicCommander.cpp
@@ -3,13 +3,12 @@
 #include "Board.hpp"
 
 namespace WarGame {
-    void ParamedicCommander::full_action(Board &board,
-                                         std::pair<int, int> placeOfSoldier) {
+    //  כמו חובש, אבל כשהוא זז, כל החובשים של אותו שחקן מרפאים את החיילים שנמצאים לידם
+    void ParamedicCommander::full_action(Board &board, std::pair<int, int> placeOfSoldier) {
         std::cout << "-= Perform paramedicSoldierS basic heal-action =-" << std::endl;
         for (int i = 0; i < board.getRows(); ++i) {
             for (int j = 0; j < board.getCols(); ++j) {
-                if (board[{i,j}] != nullptr && board[{i,j}]->playerNum ==
-                                              this->playerNum) { // if there is a soldier here and if soldier is belong to me
+                if (board[{i,j}] != nullptr && board[{i,j}]->playerNum == this->playerNum) { // אם יש חייל שם ששייך אליו
                     if (dynamic_cast<Paramedic *>(board[{i,j}])) {
                         std::cout << "Paramedic at: " << i << "," << j
                                   << " performing basic heal-action by commander order" << std::endl;
