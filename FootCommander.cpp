08845891cc248ed840fe118f1e88481630417fd8
Orgamliel7 <57177424+Orgamliel7@users.noreply.github.com>
2020-07-20 15:21:38 +0300

    Update FootCommander.cpp

--- FootCommander.cpp
+++ FootCommander.cpp
@@ -3,15 +3,16 @@
 #include "Board.hpp"
 #include <iostream>
 
-//namespace WarGame {
+//namespace WarGame { לא צריך כי 
+
+//  כשהמפקד רגליים יורה,  כל החיילים הרגליים של אותו שחקן יורים
     void WarGame::FootCommander::full_action(Board &board,
                                     std::pair<int, int> placeOfSoldier) {
         std::cout << "perform FootCommander full_action" << std::endl;
         for (int i = 0; i < board.getRows(); ++i) {
             for (int j = 0; j < board.getCols(); ++j) {
-                if (board[{i,j}] != nullptr && board[{i,j}]->playerNum ==
-                                              this->playerNum) { // if soldier belong to me
-                    if (dynamic_cast<FootSoldier *>(board[{i,j}])) {
+                if (board[{i,j}] != nullptr && board[{i,j}]->playerNum == this->playerNum) { // אם החייל שייך אליי
+                    if (dynamic_cast<FootSoldier *>(board[{i,j}])) { // נשתמש בהקצאה דינמית כדי
                         std::cout << "FootSoldier at: " << i << "," << j
                                   << " performing action" << std::endl;
                         std::pair<int, int> loc = {i, j};
